e595614863c0ab2a063e4ccecbd43e62ab07533e
Mauricio <tryptic-st@outlook.com>
2022-09-12 20:12:13 -0500

    Add filters code

--- queries.sql
+++ queries.sql
@@ -1,3 +1,15 @@
-/*Queries that provide answers to the questions from all projects.*/
+SELECT * FROM animals WHERE name LIKE '%mon';
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+SELECT * FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+
+SELECT name, date_of_birth FROM animals WHERE name = 'agumon' OR name = 'Pikachu';
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.50;
+
+SELECT * FROM animals WHERE neutered = TRUE;
+
+SELECT * FROM animals WHERE name != 'Gabumon';
+
+SELECT * FROM animals WHERE weight_kg >= 10.40 AND weight_kg <= 17.30;
