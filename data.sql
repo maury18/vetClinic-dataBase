a7e98decd5854e4612bb99f1a42090e080248ae7
Mauricio <tryptic-st@outlook.com>
2022-09-12 20:11:45 -0500

    Add values to columns

--- data.sql
+++ data.sql
@@ -1,5 +1,15 @@
-/* Populate database with sample data. */
+INSERT INTO public.animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES (1, 'agumon', '03-02-2020', 0, TRUE, 10.23);
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO public.animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES (2, 'Gabumon', '15-11-2018', 2, TRUE, 8);
+
+INSERT INTO public.animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES (3, 'Pikachu', '07-01-2021', 1, FALSE, 15.04);
+
+INSERT INTO public.animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES (4, 'Devimon', '12-05-2017', 5, TRUE, 11);
