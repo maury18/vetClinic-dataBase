387b81d9211b6f53c5c90a7df88bbd4e52f643bc
Mauricio <tryptic-st@outlook.com>
2022-09-14 14:11:43 -0500

    Create new tables and modify animals table

--- schema.sql
+++ schema.sql
@@ -15,3 +15,35 @@ ALTER TABLE IF EXISTS public.animals
 // New Column
 ALTER TABLE IF EXISTS public.animals
     ADD COLUMN species text;
+
+/* new table owners*/
+CREATE TABLE public.owners
+(
+    id bigserial,
+    full_name text,
+    age integer,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.owners
+    OWNER to postgres;
+
+/* new table species */
+CREATE TABLE public.species
+(
+    id bigserial,
+    name text,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.species
+    OWNER to postgres;
+
+/* modify animals table */
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT
+	REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT
+	REFERENCES owners(id);
